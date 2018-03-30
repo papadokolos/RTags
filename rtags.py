9fa31d69b492221bb2f31b3b79a9614bbfcfe2c2
papadokolos <papadokolos@gmail.com>
2018-03-30 15:23:14 +0300

    Added verbose setting to dynamically change logging level

--- rtags.py
+++ rtags.py
@@ -1,5 +1,18 @@
+import sublime
+
+# Import the plugin logger to initialize on plugin load
+from RTags.rtags_modules import main_logger
 
 # Import all RTags functionality commands
 from RTags.rtags_commands.follow_location import FollowLocationCommand
 from RTags.rtags_commands.find_references import FindReferencesCommand, PublishResultsToPanelCommand
 from RTags.rtags_commands.find_references_virtual_methods import FindReferencesForVirtualMethodOverridesCommand
+
+
+def plugin_loaded():
+    # Initialize the plugin's logger
+    main_logger.InitializeMainLogger()
+
+    # Re-initialize the logger on settings change
+    sublime.load_settings("RTags.sublime-settings").add_on_change(
+        "reinitialize-logger", main_logger.ReinitializeMainLogger)
--- rtags_modules/main_logger.py
+++ rtags_modules/main_logger.py
@@ -1,27 +1,46 @@
+import sublime  # To obtain plugin settings
 import logging
 
 
-def InitializeMainLogger(main_logger):
-    """Apply required settings to the main RTags logger.
-    """
-    if not main_logger.hasHandlers():
-        # Must be set to lowest level to get total freedom in handlers
-        main_logger.setLevel(logging.DEBUG)
-
-        # Disable log duplications when reloading
-        main_logger.propagate = False
-
-        # Create / override console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.WARNING)
-
-        # Create formatter and add it to the handler
-        formatter = logging.Formatter('[RTags / %(levelname)s] %(message)s')
-        console_handler.setFormatter(formatter)
-
-        # Add the handler to logger
-        main_logger.addHandler(console_handler)
-
-
+# Define the plugin logger, to be initialized on `plugin_loaded()`
 logger = logging.getLogger(__name__)
-InitializeMainLogger(logger)
+
+
+def InitializeMainLogger():
+    """ Apply required settings to the main RTags logger.
+    """
+    # Remove all previously added handlers, if any (relevant when reloading)
+    logger.handlers = []
+
+    # Must be set to lowest level to get total freedom in handlers
+    logger.setLevel(logging.DEBUG)
+
+    # Disable log duplications when reloading
+    logger.propagate = False
+
+    # Create console handler with the requested logging level
+    console_handler = logging.StreamHandler()
+    logging_level = GetRequestedLoggingLevel()
+    console_handler.setLevel(logging_level)
+
+    # Create formatter and add it to the handler
+    formatter = logging.Formatter('[RTags / %(levelname)s] %(message)s')
+    console_handler.setFormatter(formatter)
+
+    # Add the handler to logger
+    logger.addHandler(console_handler)
+
+
+def GetRequestedLoggingLevel():
+    """ Returns the appropriate logging level value by the plugin settings.
+    """
+    rtags_settings = sublime.load_settings("RTags.sublime-settings")
+    verbose_logging = rtags_settings.get("verbose")
+    return logging.DEBUG if verbose_logging else logging.WARNING
+
+
+def ReinitializeMainLogger():
+    """ Reinitializes the plugin's logger in case the settings have changed.
+    """
+    if logger.handlers[0].level != GetRequestedLoggingLevel():
+        InitializeMainLogger()
